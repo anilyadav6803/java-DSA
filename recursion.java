b04311462b5b13d1125a2ab2994220cf82f9c9e8
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-07-27 12:34:26 +0530

    recursion topic

---
+++ recursion.java
@@ -0,0 +1,18 @@
+public class recursion {
+   
+
+        public static void Decnum(int n) {
+            if (n == 1) {
+                System.out.println(n);
+                return;
+            }
+            System.out.print(n + " ");
+            Decnum(n - 1);
+        }
+    
+        public static void main(String[] args) {
+            int n = 10;
+            Decnum(n);
+        }
+    
+}
